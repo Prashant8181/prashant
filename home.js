217ad310b1391ecb7e9eab67327bb04dd6ae90f2
Prashant8181 <prashantnatekar81@gmail.com>
2026-04-26 16:32:51 +0530

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-    let logo = document.querySelector(".logo");
-
-    if (window.scrollY > 200) {
-        logo.classList.add("hide");
-    } else {
-        logo.classList.remove("hide");
-    }
-});
