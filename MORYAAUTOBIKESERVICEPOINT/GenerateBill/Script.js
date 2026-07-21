dcb89cefbacd30ad1409445d3c914ce617711464
Prashant8181 <prashantnatekar81@gmail.com>
2026-07-21 14:13:13 +0530

    Update Script.js

--- MORYAAUTOBIKESERVICEPOINT/GenerateBill/Script.js
+++ MORYAAUTOBIKESERVICEPOINT/GenerateBill/Script.js
@@ -518,5 +518,5 @@ function printBill() {
     };
 
     localStorage.setItem("billData", JSON.stringify(data));
-    window.open("/MORYAAUTOBIKESERVICEPOINT/GenerateBill/Receipt/index.html","_blank");
+    window.open("/MORYAAUTOBIKESERVICEPOINT/GenerateBill/Receipt/index.html","_blank"); 
 }
